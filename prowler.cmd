bad76c4047f0a5f381a073423526f1fbbc20e5f5
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2025-10-23 10:28:22 +0200

    Update prowler.cmd

--- prowler.cmd
+++ prowler.cmd
@@ -1,6 +1,11 @@
+Prowler is a tool for AWS Security & Compliance Assessment
+----------------------------------------------------------
 pip install prowler
 prowler --version
 prowler aws
 prowler aws -M html,json -o output/
 prowler aws -c check_mfa_enabled,check_iam_password_policy
+
+
+
   
